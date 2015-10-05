1554dc34202018f28e2f58b1ccde50ecc6ed3b1f
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-10-04 23:57:11 -0400

    Almost done with 21

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Robert von Stange on 10/4/15.
+//  Copyright (c) 2015 Robert von Stange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
